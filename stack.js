c8fb2c3fc5a81e7387c6cae27aa2614f8564ee2d
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-08-24 07:41:46 +0800

    Code Update: Added comments on the stack.js file for documentation

--- stack.js
+++ stack.js
@@ -10,12 +10,14 @@ function Stack() {
   this.count = 0;
   this.storage = {};
 
+  // adds item on top of the stack.
   this.push = function (value) {
     this.storage[this.count] = value;
     this.count++;
     console.log(`Pushed: ${value}`);
   };
 
+  // removes item from top of the stack.
   this.pop = function () {
     if (this.count === 0) {
       return undefined;
@@ -27,18 +29,22 @@ function Stack() {
     return `Popped: ${result}`;
   };
 
+  // checks the size/length of the stack.
   this.size = function () {
     return `Current size: ${this.count}`;
   };
 
+  // views the top element of the stack without removing it.
   this.peek = function () {
     return `Top element: ${this.storage[this.count - 1]}`;
   };
 
+  // checks if the stack is empty.
   this.isEmpty = function () {
     return this.count === 0;
   };
 
+  // clears the stack.
   this.clear = function () {
     this.count = 0;
     this.storage = {};
@@ -62,4 +68,22 @@ if (typeof module !== "undefined" && require.main === module) {
   console.log(stack.pop());
   console.log(stack.peek());
   console.log(stack.size());
+  console.log(stack.isEmpty());
+  stack.clear();
+  console.log(stack.size());
+
+  /**
+   * OUTPUT:
+   * Pushed: Luderio
+   * Pushed: Chezzy
+   * Pushed: New Item
+   * Current size: 3
+   * Top element: New Item
+   * Popped: New Item
+   * Top element: Chezzy
+   * Current size: 2
+   * false
+   * Stack cleared
+   * Current size: 0
+   */
 }
