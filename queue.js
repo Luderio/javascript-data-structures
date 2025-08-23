d482dd9804bfe2cfa6544636e4c5b2af79db2bf0
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-08-23 19:50:29 +0800

    Code Update: Added comments for documentation

--- queue.js
+++ queue.js
@@ -1,32 +1,43 @@
-function Queue() {
-  collection = [];
+// Manual implemettaion of Queue Data Structure
 
+// declared a Queue Function Constructor.
+function Queue() {
+  collection = []; // Initialize the collection array
+
+  // you can remove the console.log() on each method. I only added it to log the result on the terminal.
+
+  // will return the current state of the queue.
   this.print = function () {
     console.log(`Queue Items: ${collection}`);
     return collection;
   };
 
+  // will add an item to the end of the queue.
   this.enqueue = function (element) {
     collection.push(element);
     console.log(`Enqueued: ${element}`);
   };
 
+  // will remove an item from the front of the queue.
   this.dequeue = function () {
     let removed = collection.shift();
     console.log(`Dequeued: ${removed}`);
     return removed;
   };
 
+  // will display the first item on the queue.
   this.front = function () {
     console.log(`Front Item: ${collection[0]}`);
     return collection[0];
   };
 
+  // will display the size
   this.size = function () {
     console.log(`Queue Size: ${collection.length}`);
     return collection.length;
   };
 
+  // to check if the queue is empty.
   this.isEmpty = function () {
     console.log(collection.length === 0);
     return collection.length === 0;
@@ -42,3 +53,15 @@ queue.print();
 queue.dequeue();
 queue.front();
 queue.print();
+queue.isEmpty();
+
+/**  OUTPUT:
+Enqueued: Luderio
+Enqueued: Sanchez
+Enqueued: New Item
+Queue Items: Luderio,Sanchez,New Item
+Dequeued: Luderio
+Front Item: Sanchez
+Queue Items: Sanchez,New Item
+false
+*/
