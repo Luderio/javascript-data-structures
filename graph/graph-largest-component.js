6b6130f8bc3c61bd2ea46b430451c4895e2e7eef
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-13 20:33:42 +0800

    Code Update: completed the code for the largest component

--- graph/graph-largest-component.js
+++ graph/graph-largest-component.js
@@ -1,18 +1,54 @@
-function largest_component(graph) {
-  let set = new Set();
-  const islands = [];
+function traverse(graph, node, set) {
+  if (
+    node === undefined ||
+    typeof set !== "object" ||
+    typeof graph !== "object" ||
+    Object.keys(graph).length === 0
+  ) {
+    return 0;
+  }
 
+  if (set.has(String(node))) {
+    return false;
+  } else {
+    set.add(String(node));
+  }
+
+  const neighbors = graph[node];
+  let size = 1;
+
+  while (neighbors.length > 0) {
+    const current = neighbors.pop();
+    size += traverse(graph, current, set);
+  }
+
+  return size;
+}
+
+function largest_component(graph) {
   if (typeof graph !== "object" || Object.keys(graph).length === 0) {
     return;
   }
+  let set = new Set();
+  let largest = 0;
+
+  for (let node of Object.keys(graph)) {
+    let size = traverse(graph, node, set);
+    if (size > largest) largest = size;
+  }
+  return largest;
 }
 
-function traverse(graph, node, set) {
-  if (
-    typeof graph !== "object" ||
-    typeof set !== "object" ||
-    node === undefined
-  ) {
-    return;
-  }
-}
+const graph = {
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1],
+};
+
+const largest_island = largest_component(graph);
+console.log(largest_island);
