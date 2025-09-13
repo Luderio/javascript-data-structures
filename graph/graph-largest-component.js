13f105054d39ff27ea3aa006c08e7a2cdd9559e6
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-13 18:45:28 +0800

    Code Update: Added input validation on the functions

--- graph/graph-largest-component.js
+++ graph/graph-largest-component.js
@@ -1,9 +1,18 @@
 function largest_component(graph) {
   let set = new Set();
+  const islands = [];
 
   if (typeof graph !== "object" || Object.keys(graph).length === 0) {
-    return 0;
+    return;
   }
 }
 
-function traverse(graph, node, set) {}
+function traverse(graph, node, set) {
+  if (
+    typeof graph !== "object" ||
+    typeof set !== "object" ||
+    node === undefined
+  ) {
+    return;
+  }
+}
