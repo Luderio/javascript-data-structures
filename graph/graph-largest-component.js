a876897037a64e9e9088f954efb3c844a9ae7a3b
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-13 18:41:17 +0800

    New File: Created a new file called graph-largest-component.js

---
+++ graph/graph-largest-component.js
@@ -0,0 +1,9 @@
+function largest_component(graph) {
+  let set = new Set();
+
+  if (typeof graph !== "object" || Object.keys(graph).length === 0) {
+    return 0;
+  }
+}
+
+function traverse(graph, node, set) {}
