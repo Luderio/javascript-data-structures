8a3c5f84c07afc0bc70b5b96cfd4667de4dcfc91
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-13 12:46:49 +0800

    Code Update: Tried another logic approach on the rcursive function

--- graph/graph-connected-components-count.js
+++ graph/graph-connected-components-count.js
@@ -26,7 +26,11 @@ function graph_island_count(graph) {
       set.add(String(node));
     }
 
-    while (neighbors.length > 0) {
+    // while (neighbors.length > 0) {
+    //   const neighbor = neighbors.shift();
+    //   traverse(graph, neighbor, set);
+    // }
+    if (neighbors.length > 0) {
       const neighbor = neighbors.shift();
       traverse(graph, neighbor, set);
     }
