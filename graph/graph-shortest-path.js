7606d58a970be62bb26112c971401b98d944d625
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-16 18:37:55 +0800

    Code Fix: Able to fix the logic issue by dividing the edge by 2 and getting the ceiling of the divident

--- graph/graph-shortest-path.js
+++ graph/graph-shortest-path.js
@@ -36,18 +36,19 @@ function isSet(set) {
 }
 
 function traverse(graph, nodeA, nodeB, set, edge) {
-  if (!isPlainObject(graph) || !isSet(set)) {
+  if (
+    !isPlainObject(graph) ||
+    !isSet(set) ||
+    set.size === Object.keys(graph).length
+  ) {
     return -1;
   }
 
   const neighbors = [...nodeA];
   const current_node = neighbors.shift();
-  if (set.size === Object.keys(graph).length) {
-    return -1;
-  }
 
   if (current_node === nodeB) {
-    return edge - 1; // look into this code.
+    return Math.ceil(edge / 2);
   }
 
   if (set.has(current_node)) {
@@ -70,6 +71,8 @@ function shortestPath(edges, nodeA, nodeB) {
   const graph = build_graph(edges);
   let set = new Set();
   let edge = 0;
+  console.log(graph);
+
   return traverse(graph, nodeA, nodeB, set, edge);
 }
 
@@ -81,5 +84,5 @@ const edges = [
   ["w", "v"],
 ];
 
-const shortest_path = shortestPath(edges, "w", "x");
+const shortest_path = shortestPath(edges, "v", "y");
 console.log(shortest_path);
