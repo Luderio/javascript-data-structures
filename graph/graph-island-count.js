9cd25050e5a20563c65302730c2c6db2c409c7a3
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-17 09:57:35 +0800

    Code Update: Added the coding challenge instructions

---
+++ graph/graph-island-count.js
@@ -0,0 +1,7 @@
+/**
+ * Graph: Island Count
+ *
+ * Write a function islandCount(), that takes in a grid containing Ws and Ls. W represents water and L represents land.
+ * The function should return the number of islands on the grid.
+ * An island is a vertically or horizontally connected region of the land.
+ */
