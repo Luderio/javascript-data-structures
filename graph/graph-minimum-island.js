247c715fe0a11c7ff357f4b1a0ae9f67adae1107
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-18 12:49:02 +0800

    Code Update: Reverting to version 1 of the code

--- graph/graph-minimum-island.js
+++ graph/graph-minimum-island.js
@@ -1,5 +1,5 @@
 /**
- * Graph: Minimum Island
+ * Graph: Minimum Island Version 1
  * Write a function minimumIsland(), that takes in a grid containing Ws and Ls.
  * W represents water and L represents land.
  * The function should return the size of the smallest island.
@@ -9,41 +9,44 @@
 
 const minimumIsland = (grid) => {
   const visited = new Set();
-  let minumun_size = Infinity;
+  let land = [];
+  let size = [];
   for (let row = 0; row < grid.length; row++) {
     for (let column = 0; column < grid[0].length; column++) {
-      const size = sail(grid, row, column, visited);
-      if (size > 0 && size < minumun_size) {
-        minumun_size = size;
+      if (sail(grid, row, column, land, visited) === true) {
+        let island_size = land.length;
+        size.push(island_size);
+        land = [];
       }
     }
   }
 
-  return minumun_size;
+  return Math.min(...size);
 };
 
-const sail = (grid, row, column, visited) => {
+const sail = (grid, row, column, land, visited) => {
   const validRowCoordinate = 0 <= row && row < grid.length;
   const validColumnCoordinate = 0 <= column && column < grid[0].length;
 
-  if (!validRowCoordinate || !validColumnCoordinate) return 0;
-  if (!Array.isArray(grid) || !visited.constructor === Set) return 0;
-  if (grid[row][column] === "W" || grid[row][column] === "w") return 0;
+  if (!validRowCoordinate || !validColumnCoordinate) return false;
+  if (!Array.isArray(grid) || !visited.constructor === Set) return false;
+  if (grid[row][column] === "W" || grid[row][column] === "w") return false;
 
   const coordinates = `${row},${column}`;
 
-  if (visited.has(coordinates)) return 0;
+  if (grid[row][column] === "L" && !visited.has(coordinates))
+    land.push(grid[row][column]);
+
+  if (visited.has(coordinates)) return false;
   visited.add(coordinates);
 
-  let size = 1;
-
   // this recursive calls will only be called on L nodes that is not yet added to the visited set.
-  size += sail(grid, row - 1, column, visited);
-  size += sail(grid, row + 1, column, visited);
-  size += sail(grid, row, column - 1, visited);
-  size += sail(grid, row, column + 1, visited);
+  sail(grid, row - 1, column, land, visited);
+  sail(grid, row + 1, column, land, visited);
+  sail(grid, row, column - 1, land, visited);
+  sail(grid, row, column + 1, land, visited);
 
-  return size;
+  return true;
 };
 
 const grid = [
