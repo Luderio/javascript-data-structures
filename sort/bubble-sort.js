7c7423b243d490c6adab2f8399bca0cf84f89346
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-18 14:52:37 +0800

    Completed coding the bubble sort algorithm

---
+++ sort/bubble-sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(data) {
+  let len = data.length;
+
+  do {
+    for (let i = 0; i < len - 1; i++) {
+      if (data[i] > data[i + 1]) {
+        [([data[i], data[i + 1]] = [data[i + 1], data[i]])];
+      }
+    }
+  } while (len--);
+
+  return data;
+}
+
+const unsorted_list = [7, 4, 6, 5, 2, 9, 3, 8, 1];
+console.log(bubbleSort(unsorted_list));
