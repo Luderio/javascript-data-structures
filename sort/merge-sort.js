96708b847ccaf04e84110d74c54c4c06702b04c3
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-18 14:36:32 +0800

    Code update: Completed coding the merge sort algorithm

--- sort/merge-sort.js
+++ sort/merge-sort.js
@@ -2,3 +2,33 @@
  * Merge Sort
  *
  */
+
+function merge(left, right) {
+  const out = [];
+
+  while (left.length && right.length) {
+    out.push(left[0] < right[0] ? left.shift() : right.shift());
+  }
+
+  while (left.length) {
+    out.push(left.shift());
+  }
+
+  while (right.length) {
+    out.push(right.shift());
+  }
+
+  return out;
+}
+function mergeSort(data) {
+  if (data.length < 2) return data;
+
+  const mid_point = Math.round(data.length / 2);
+  const left = mergeSort(data.slice(0, mid_point));
+  const right = mergeSort(data.slice(mid_point));
+
+  return merge(left, right);
+}
+
+const unsorted_array = [7, 4, 6, 5, 2, 9, 3, 8, 1];
+console.log(mergeSort(unsorted_array));
